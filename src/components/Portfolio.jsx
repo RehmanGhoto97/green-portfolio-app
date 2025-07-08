660f3af27d118f5fe1f586b287a901ed7e82de45
RehmanGhoto97 <mrehmanfreelancer02@gmail.com>
2025-07-09 03:32:56 +0500

    Update Portfolio.jsx
    portfolio img updated

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
-import slidImg1 from './../assets/images/dctr.JPG'
-import slidImg2 from './../assets/images/hotl.JPG'
-import slidImg3 from './../assets/images/p1.PNG'
-import slidImg4 from './../assets/images/portfolio1.JPG'
-import slidImg5 from './../assets/images/portfolio3.PNG'
+import slidImg1 from './../assets/images/dctr.jpg'
+import slidImg2 from './../assets/images/hotl.jpg'
+import slidImg3 from './../assets/images/p1.png'
+import slidImg4 from './../assets/images/portfolio1.jpg'
+import slidImg5 from './../assets/images/portfolio3.png'
 import { MdArrowOutward } from 'react-icons/md';
 import { FaGithub } from 'react-icons/fa';
 import { Helmet } from 'react-helmet';
